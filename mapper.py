12c8882b3e66dab757f6df265bd5c0839c982416
cody-mar10 <ccmartin6@wisc.edu>
2023-01-16 14:35:31 -0600

    added a batch mode that accepts a file that contains multiple references with the corresponding reads

--- mapper.py
+++ mapper.py
@@ -3,14 +3,15 @@ import argparse
 import logging
 import sys
 from pathlib import Path
-from typing import List
+from typing import List, Optional
 import resource
 
 import bowtie
 import bwa
-from utils import check_installs
+from utils import check_installs, read_batch_file
 
 DISPATCH_TABLE = {"bowtie2": bowtie.main, "bwa": bwa.main}
+
 
 def parse_args() -> argparse.Namespace:
     # TODO: probably should have subparsers for each mapper...
@@ -24,25 +25,50 @@ def parse_args() -> argparse.Namespace:
     )
     # ubparsers = parser.add_subparsers(dest="mapper")
     # bwa_parser = bwa.get_bwa_argparser()
-    parser.add_argument(
+    io_args = parser.add_argument_group("IO ARGS")
+    reads_args = io_args.add_mutually_exclusive_group(required=True)
+    batch_args = parser.add_argument_group("BATCH MODE ARGS")
+
+    io_args.add_argument(
         "-i",
         "--input",
         required=True,
         help="input reference genome file in fasta format",
     )
-    parser.add_argument(
+    reads_args.add_argument(
         "-r",
         "--reads",
-        required=True,
         nargs="+",
-        help="path or glob pattern to the reads. Ex: /path/to/reads/*.fastq.gz",
+        help="path or glob pattern to the reads. Ex: /path/to/reads/*.fastq.gz -- Mutually exclusive with --batch",
     )
-    parser.add_argument(
+    reads_args.add_argument(
+        "--batch",
+        default=False,
+        action="store_true",
+        help="use if -i file is a tab-delimited file that maps assemblies (fasta files) to all reads that need to be mapped to the reference -- Mutually exclusive with -r/--reads",
+    )
+    io_args.add_argument(
         "-o",
         "--outdir",
         default=Path.cwd().resolve(),
         help="output directory for bam files (default: %(default)s)",
     )
+
+    batch_args.add_argument(
+        "-fd",
+        "--ref-dir",
+        help="directory that contains all reference fasta files (assemblies). If not provided, assumes that the batch file has the correct paths",
+    )
+    batch_args.add_argument(
+        "-rd",
+        "--reads-dir",
+        help="directory that contains all reads. If not provided, assumes that the batch file has the correct paths",
+    )
+    batch_args.add_argument(
+        "-no-header",
+        help="use if your batch file provided in -i with --batch has no header",
+    )
+
     parser.add_argument(
         "-t",
         "--threads",
@@ -67,27 +93,7 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
-def main(
-    reference: Path,
-    reads: List[str],
-    mapper: str,
-    threads: int,
-    outdir: Path,
-    log: Path,
-) -> None:
-    """_summary_
-
-    Args:
-        reference (Path): _description_
-        reads (List[str]): _description_
-        mapper (str): _description_
-        threads (int): _description_
-        outdir (Path): _description_
-        log (Path): _description_
-
-    Raises:
-        NotImplementedError: _description_
-    """
+def setup(outdir: Path, log: Path, mapper: str):
     outdir.mkdir(exist_ok=True, parents=True)
     logging.basicConfig(
         filename=log,
@@ -101,11 +107,73 @@ def main(
     elif mapper == "bowtie2":
         logging.info(f"Running bowtie2 for mapping")
 
+
+def _main(mapper: str, reference: Path, reads: List[str], threads: int, outdir: Path):
     mapping_fn = DISPATCH_TABLE[mapper]
     mapping_fn(reference, reads, threads, outdir)
 
-    maxmem_GB = resource.getrusage(resource.RUSAGE_CHILDREN).ru_maxrss / (1024 ** 2)
+    maxmem_GB = resource.getrusage(resource.RUSAGE_CHILDREN).ru_maxrss / (1024**2)
     logging.info(f"Max memory usage: {maxmem_GB:.3f} GB")
+
+
+def main(
+    reference: Path,
+    reads: List[str],
+    mapper: str,
+    threads: int,
+    outdir: Path,
+    log: Path,
+) -> None:
+    """single reference mode
+
+    Args:
+        reference (Path): _description_
+        reads (List[str]): _description_
+        mapper (str): _description_
+        threads (int): _description_
+        outdir (Path): _description_
+        log (Path): _description_
+
+    Raises:
+        NotImplementedError: _description_
+    """
+    setup(outdir, log, mapper)
+    _main(mapper, reference, reads, threads, outdir)
+
+
+def main_batch(
+    batchfile: Path,
+    mapper: str,
+    threads: int,
+    outdir: Path,
+    log: Path,
+    header: bool = True,
+    refdir: Optional[Path] = None,
+    readsdir: Optional[Path] = None,
+) -> None:
+    """batch reference mode
+
+    Args:
+        batchfile (Path): _description_
+        mapper (str): _description_
+        threads (int): _description_
+        outdir (Path): _description_
+        log (Path): _description_
+
+    Raises:
+        NotImplementedError: _description_
+    """
+    setup(outdir, log, mapper)
+
+    for reference, reads in read_batch_file(batchfile, header):
+        # samtools_view_and_sort calls proc.wait() so won't open all at once
+        # actually need to make the mapping step wait
+        if refdir is not None:
+            reference = refdir.joinpath(reference)
+
+        if readsdir is not None:
+            reads = [readsdir.joinpath(read).as_posix() for read in reads]
+        _main(mapper, reference, reads, threads, outdir)
 
 
 if __name__ == "__main__":
@@ -113,11 +181,25 @@ if __name__ == "__main__":
     check_installs(args.mapper, "samtools")
 
     outdir = Path(args.outdir)
-    main(
-        reference=Path(args.input),
-        reads=args.reads,
-        mapper=args.mapper,
-        threads=args.threads,
-        outdir=outdir,
-        log=outdir.joinpath(args.log),
-    )
+
+    if args.batch:
+
+        main_batch(
+            batchfile=Path(args.input),
+            mapper=args.mapper,
+            threads=args.threads,
+            outdir=outdir,
+            log=outdir.joinpath(args.log),
+            header=(not args.no_header),
+            refdir=None if args.ref_dir is None else Path(args.ref_dir),
+            readsdir=None if args.reads_dir is None else Path(args.reads_dir),
+        )
+    else:
+        main(
+            reference=Path(args.input),
+            reads=args.reads,
+            mapper=args.mapper,
+            threads=args.threads,
+            outdir=outdir,
+            log=outdir.joinpath(args.log),
+        )
